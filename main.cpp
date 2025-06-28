69e0f2902b7aa6b2607f8b827de1301fa8eec4ee
emorywalrus <72038374+emorywalrus@users.noreply.github.com>
2025-06-28 17:38:00 -0500

    update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include "matrix.h"
 
+void clear() {
+    std::cout << "\033[2J\033[1;1H";
+}
+
 int main(int argc, char** argv) {
-    matrix<10, 10> t1;
+    clear();
+    matrix t1;
     t1.print();
     return 0;
 }
--- matrix.h
+++ matrix.h
@@ -5,25 +5,79 @@
 #include <functional>
 #include <sstream>
 #include <iomanip>
+#include <cstring>
 
 
 using namespace std;
 
-template <int width, int height>
-struct matrix : array<array<float, height>, width> {
-    matrix() : array<array<float, height>, width>() {}
+template<typename T>
+concept Number = integral<T> || floating_point<T>;
 
-    void loop(function<void(int, int)> f) const {
-        for (int y = 0; y < width; ++y) {
+template <int width = 4, int height = 4, typename T = float>
+class matrix : array<array<T, height>, width> {
+    public:
+    matrix() : array<array<T, height>, width>() {}
+
+    matrix(function<T()> f) : array<array<T, height>, width>() {
+        loop([&](int x, int y) {
+            (*this)[x][y] = f();
+        });
+    }
+
+    matrix(const matrix& other) : array<array<T, height>, width>() {
+        loop([&](int x, int y) {
+            (*this)[x][y] = other[x][y];
+        });
+    }
+
+    matrix(function<T(int x, int y)> f) : array<array<T, height>, width>() {
+        loop([&](int x, int y) {
+            (*this)[x][y] = f(x, y);
+        });
+    }
+
+    matrix<width, height, T> copy() {
+        return matrix<width, height, T>(*this);
+    }
+
+    void loop(function<void(int x, int y)> f) const {
+        for (int y = 0; y < height; ++y) {
             for (int x = 0; x < width; ++x) {
                 f(x, y);
             }
         }
     }
 
-    matrix operator+(const matrix& other) const {
-        matrix ret;
-        loop()
+    void loop(function<void()> f) const {
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                f();
+            }
+        }
+    }
+
+    matrix<width, height, T> operator+(const matrix& other) const {
+        return matrix([&](int x, int y) {
+            return (*this)[x][y] + other[x][y];
+        });
+    }
+
+    template<int other_width, int other_height, typename T>
+    matrix<other_width, height, T> operator*(const matrix<other_width, other_height, T>& other) const {
+        return matrix<other_width, height, T>([&](int x, int y) {
+            float ret = 0;
+            for (int i = 0; i < width; ++i) {
+                ret += (*this)[i][y] * other[x][i];
+            }
+            return ret;
+        });
+    }
+
+    template<Number N>
+    matrix<width, height, T> operator*(const N& n) const {
+        return matrix<width, height, T>([&](int x, int y) {
+            return (*this)[x][y] * n;
+        });
     }
 
     operator string() const {
